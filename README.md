8db5ac9ec8d5590b4a2b78a41abc4c41a76245eb
Týnovský Miroslav <tynovsky@avast.com>
2014-11-27 12:49:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Unpack::SevenZip - It's new $module
+
+# SYNOPSIS
+
+    use Unpack::SevenZip;
+
+# DESCRIPTION
+
+Unpack::SevenZip is ...
+
+# LICENSE
+
+Copyright (C) Týnovský Miroslav.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Týnovský Miroslav <tynovsky@avast.com>
---
+++ lib/Unpack/SevenZip.pm
@@ -0,0 +1,39 @@
+package Unpack::SevenZip;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Unpack::SevenZip - It's new $module
+
+=head1 SYNOPSIS
+
+    use Unpack::SevenZip;
+
+=head1 DESCRIPTION
+
+Unpack::SevenZip is ...
+
+=head1 LICENSE
+
+Copyright (C) Týnovský Miroslav.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Týnovský Miroslav E<lt>tynovsky@avast.comE<gt>
+
+=cut
+
